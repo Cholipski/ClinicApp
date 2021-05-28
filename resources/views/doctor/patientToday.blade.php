bfb5c463aff9e5aa6d3b0a9b6a023a058b7da9fd
Pawel Bronk <150788@student.uwm.edu.pl>
2021-05-28 18:35:04 +0200

    Update appointment doctor view and validation

--- resources/views/doctor/patientToday.blade.php
+++ resources/views/doctor/patientToday.blade.php
@@ -1,30 +1,41 @@
 @extends('admin.layouts.master')
 @section('content')
+    @if (Session::has('message'))
+        <div class="alert alert-success">
+            {{ Session::get('message') }}
+        </div>
+    @endif
+    @if (Session::has('errmessage'))
+        <div class="alert alert-danger">
+            {{ Session::get('errmessage') }}
+        </div>
+    @endif
     <div class="container-fluid">
         <div class="card">
             <div class="card-body">
                 <table id="patient_table" class="table">
                     <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Imię</th>
-                        <th>Nazwisko</th>
-                        <th>Godzina</th>
-                        <th>Wizyta</th>
-                    </tr>
+                        <tr>
+                            <th>Id</th>
+                            <th>Imię</th>
+                            <th>Nazwisko</th>
+                            <th>Godzina</th>
+                            <th>Wizyta</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    @foreach($patients as $patient)
-                        <tr>
-                            <td>{{$patient->id}}</td>
-                            <td>{{$patient->name}}</td>
-                            <td>{{$patient->last_name}}</td>
-                            <td>{{$patient->time}}</td>
-                            <td>
-                                <a href="{{ route('doctor.appointment',$patient->id) }}" class="btn btn-dark">Rozpocznij</a>
-                            </td>
-                        </tr>
-                    @endforeach
+                        @foreach ($patients as $patient)
+                            <tr>
+                                <td>{{ $patient->id }}</td>
+                                <td>{{ $patient->name }}</td>
+                                <td>{{ $patient->last_name }}</td>
+                                <td>{{ $patient->time }}</td>
+                                <td>
+                                    <a href="{{ route('doctor.appointment', $patient->id) }}"
+                                        class="btn btn-dark">Rozpocznij</a>
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
