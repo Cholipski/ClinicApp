b5c976f2c43072e2485e6dc387f7e97db1b16478
Kamil Chomej <klifer9999@gmail.com>
2021-06-08 16:25:25 +0200

    Add patient card layout
    Add patient information panel, alergies panel, list of visits and prescription list

--- resources/views/doctor/patientCard.blade.php
+++ resources/views/doctor/patientCard.blade.php
@@ -1,42 +1,120 @@
 @extends('admin.layouts.master')
 @section('content')
-    @foreach ($patients as $patient)
-        <tr>
-            imie <td>{{ $patient->first_name }}</td><br>
-           nazwisko <td>{{ $patient->last_name }}</td><br>
-           adres  <td>{{ $patient->address }}</td><br>
-            tel <td>{{ $patient->phone_number }}</td><br>
-            alergia <td>{{ $patient->alergies }}</td><br>
-            leki <td>{{ $patient->medicaments }}</td><br>
-        </tr>
-    @endforeach
-<b>Wizyty</b><br>
-    @foreach ($bookings as $booking)
-        <tr>
-            data <td>{{ $booking->date }}</td><br>
-           czas  <td>{{ $booking->time }}</td><br>
-           objawy  <td>{{ $booking->symptoms }}</td><br>
-        </tr>
-    @endforeach
-<b>recepty</b><br>
-    @if ($prescriptions->count() > 0)
-        @foreach ($prescriptions as $prescription)
-            <tr>
-              data <td>{{ $prescription->invoice_date }}</td><br>
-               kod  <td>{{ $prescription->access_code }}</td><br>
-                recepta <td>
 
-                    <button data-toggle="modal" data-data="{{ $prescription }}"
-                            data-target="#showPrescriptionModal{{ $prescription->id }}"
-                            class="btn btn-dark btnShowPrescription">Pokaż
-                        receptę</button>
-                </td><br>
-            </tr><br>
-            @include('doctor.prescriptionDetail',['doctor'=>auth()->user()])
-        @endforeach
-    @else
-        <td>Brak wystawionych recept</td>
-    @endif
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="card text-center" style="min-height: 422px">
+                    <div class="card-header justify-content-center">
+                        <h3>Podstawowe informacje o pacjencie</h3>
+                    </div>
+                    <div class="card-body d-flex justify-content-around align-items-center">
+                        <div class="row w-100 justify-content-around">
+                            <div class="col-md-6">
+                                <img class="rounded-circle" width="150px" height="184px"
+                                    src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" />
+                            </div>
+                            <div class="col-md-6">
+                                @foreach ($patients as $patient)
+                                    <p style="margin-top: 1rem">Imię: {{ $patient->first_name }}</p>
+                                    <p>Naziwsko : {{ $patient->last_name }}</p>
+                                    <p>Numer telefonu: {{ $patient->phone_number }}</p>
+                                    <p>Adres zamieszkania: {{ $patient->address }}</p>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-lg-4">
+                <div class="card text-center" style="min-height: 422px">
+                    <div class="card-header justify-content-center">
+                        <h3>Alergie pacjenta</h3>
+                    </div>
+                    <div class="card-body d-flex justify-content-center align-items-center" style="flex-wrap:nowrap">
+                        @foreach ($patients as $patient)
+                            <p>{{ $patient->alergies }}</p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="card" style="min-height: 300px">
+                    <div class="card-header justify-content-center">
+                        <h3>Podstawowe informacje o pacjencie</h3>
+                    </div>
+                    <div class="card-body">
+
+                        <div class="table-responsive">
+                            <table class="table table-hover" style="margin-top: 2.5rem">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>Data</th>
+                                        <th>Czas</th>
+                                        <th>Objawy</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($bookings as $booking)
+                                        <tr>
+                                            <td>{{ $booking->date }}</td>
+                                            <td>{{ $booking->time }}</td>
+                                            <td>{{ $booking->symptoms }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="card" style="min-height: 300px">
+                    <div class="card-header justify-content-center">
+                        <h3>Podstawowe informacje o pacjencie</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover" style="margin-top: 2.5rem">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th>Data</th>
+                                        <th>Kod</th>
+                                        <th>Recepta</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @if ($prescriptions->count() > 0)
+                                        @foreach ($prescriptions as $prescription)
+                                            <tr>
+                                                <th style="font-weight: normal">
+                                                    Data: {{ $prescription->invoice_date }}
+                                                </th>
+                                                <th style="font-weight: normal">
+                                                    Kod: {{ $prescription->access_code }}
+                                                </th>
+                                                <th style="font-weight: normal">
+                                                    <button data-toggle="modal" data-data="{{ $prescription }}"
+                                                        data-target="#showPrescriptionModal{{ $prescription->id }}"
+                                                        class="btn btn-dark btnShowPrescription">Wyświetl receptę</button>
+                                                    @include('doctor.prescriptionDetail',['doctor'=>auth()->user()])
+                                                </th>
+                                            </tr>
+                                        @endforeach
+                                    @else
+                                        <p>Brak wystawionych recept</p>
+                                    @endif
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
-
